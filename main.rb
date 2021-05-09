eee3e8cd22bd8d0811bcb190097bedf4bd3386ce
Carson-Key <keyc@oregonstate.edu>
2021-05-09 15:26:14 -0700

    Rename label_text

--- main.rb
+++ main.rb
@@ -1,9 +1,11 @@
 def draw_button(label_text, x, y, foreground_color, is_dark_mode)
+    button_text = label_text
+
     if is_dark_mode
         # darken foreground color for dark mode
-        paint(label_text, x, y, foreground_color - 10, '#111111')
+        paint(button_text, x, y, foreground_color - 10, '#111111')
     else
         # lighten foreground color for non-dark mode
-        paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+        paint(button_text, x, y, foreground_color + 10, '#E0E0E0')
     end
 end
