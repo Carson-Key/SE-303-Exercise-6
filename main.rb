45dc64a1484a224b7772637389ecbd8af1080e1d
Carson-Key <keyc@oregonstate.edu>
2021-05-09 15:28:30 -0700

    Extract dark_mode conditional

--- main.rb
+++ main.rb
@@ -1,3 +1,13 @@
+def is_dark_mode()
+    if is_dark_mode
+        # darken foreground color for dark mode
+        paint(button_text, x, y, foreground_color - 10, '#111111')
+    else
+        # lighten foreground color for non-dark mode
+        paint(button_text, x, y, foreground_color + 10, '#E0E0E0')
+    end
+end
+
 def draw_button(label_text, x, y, foreground_color, is_dark_mode)
     button_text = label_text
 
