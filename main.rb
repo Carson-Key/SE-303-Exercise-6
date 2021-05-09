5add8668cddcf047cc360bb83363d013f1201724
Carson-Key <keyc@oregonstate.edu>
2021-05-09 15:34:18 -0700

    Add missing parameters

--- main.rb
+++ main.rb
@@ -1,10 +1,10 @@
-def is_dark_mode()
+def is_dark_mode(button_text, original_foreground_color, is_dark_mode)
     if is_dark_mode
         # darken foreground color for dark mode
-        paint(button_text, x, y, foreground_color - 10, '#111111')
+        paint(button_text, x, y, original_foreground_color - 10, '#111111')
     else
         # lighten foreground color for non-dark mode
-        paint(button_text, x, y, foreground_color + 10, '#E0E0E0')
+        paint(button_text, x, y, original_foreground_color + 10, '#E0E0E0')
     end
 end
 
