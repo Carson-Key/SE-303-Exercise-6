7a33bd4471febe63d492de0973d92869f284133b
Carson-Key <keyc@oregonstate.edu>
2021-05-09 15:45:22 -0700

    Assign button_color to reurn of is_dark_mode

--- main.rb
+++ main.rb
@@ -21,6 +21,8 @@ def draw_button(label_text, x, y, foreground_color, is_dark_mode)
     button_text = label_text
     original_foreground_color = foreground_color
 
+    button_color = is_dark_mode(original_foreground_color, is_dark_mode)
+
     if is_dark_mode
         # darken foreground color for dark mode
         paint(button_text, x, y, foreground_color - 10, '#111111')
